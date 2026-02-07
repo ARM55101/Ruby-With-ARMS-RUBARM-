6ff6509c07ab857e1a02cf9c3720f1880ab7fc59
ARM55 <debianmicro@gmail.com>
2026-02-07 17:40:57 +0000

    Update runtime.sh

--- Runtime/runtime.sh
+++ Runtime/runtime.sh
@@ -1,6 +1,10 @@
 //RubARM//
 //The following code is under the BSD2 licence//
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install ruby
+brew install brew install --cask gcc-arm-embedded
+brew install arm-none-eabi-gcc
 for file in *.rb; do as "$file"; done
 ruby run.rb
-
+install arm-none-eabi-gcc -Z -o program.o input.s
 
